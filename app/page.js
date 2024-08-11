d3edfc97a291fa5fdc40f3fa8b8f675a3d7a3543
Deepu Cyriac <deepucvk123@gmail.com>
2024-08-11 19:14:35 +0530

    Added login server action for validating user credentials

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
-import AuthForm from '@/components/auth-form';
+import AuthForm from "@/components/auth-form";
 
-export default async function Home() {
-  return <AuthForm />;
+//searchParams is a reserved prop which contains an object with query parameters
+export default async function Home({ searchParams }) {
+  const formMode = searchParams.mode || "login";
+
+  return <AuthForm mode={formMode} />;
 }
